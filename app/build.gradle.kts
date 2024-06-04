98e5e3caa8796f7ebcc9002702f6cb6a82848700
Anders Haahr <anders.haahr@jayway.com>
2024-06-04 11:18:57 +0200

    Cleaned the dependency list

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,28 +56,44 @@ android {
 }
 
 dependencies {
+    // Android core
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.1")
     implementation("androidx.activity:activity-compose:1.9.0")
+
+    // Compose
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.navigation:navigation-compose:2.8.0-beta02")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+
+    // Dependency Injection
     implementation("com.google.dagger:hilt-android:2.48")
-    implementation("com.github.bumptech.glide:compose:1.0.0-beta01")
     kapt("com.google.dagger:hilt-android-compiler:2.48")
-    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.2")
+
+    // Images
+    implementation("com.github.bumptech.glide:compose:1.0.0-beta01")
+
+    // Navigation
+    implementation("androidx.navigation:navigation-compose:2.8.0-beta02")
+
+    // Network
     api("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.2")
+
+    // Serialization
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // Testing
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+
+    // Compose Tools
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 }
