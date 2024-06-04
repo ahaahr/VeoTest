0a5ae9ea8ebce5f22cd550f62a0cb7da44bbf041
Anders Haahr <anders.haahr@jayway.com>
2024-06-04 15:01:19 +0200

    Some minor cleanup

--- app/src/main/java/co/veo/veotest/MainActivity.kt
+++ app/src/main/java/co/veo/veotest/MainActivity.kt
@@ -4,10 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -49,21 +45,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    VeoTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/co/veo/veotest/network/ApiService.kt
+++ app/src/main/java/co/veo/veotest/network/ApiService.kt
@@ -7,18 +7,20 @@ import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Path
 
+private const val token = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NTcwYmM0MjgxY2RkZDdlNTNiNzYxY2ZlYjg0OTIyNyIsInN1YiI6IjY2NWRiOWQ1YTNmYTViYWM3MDQ5MWVlZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kyfNUr5KGnjwb-1j5oTN1637XR0raGTixdo_4IpZ-u8"
+
 interface ApiService {
 
     @GET("trending/all/week")
     fun getTrending(
         @Header("accept") accept: String = "application/json",
-        @Header("Authorization") authToken: String = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NTcwYmM0MjgxY2RkZDdlNTNiNzYxY2ZlYjg0OTIyNyIsInN1YiI6IjY2NWRiOWQ1YTNmYTViYWM3MDQ5MWVlZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kyfNUr5KGnjwb-1j5oTN1637XR0raGTixdo_4IpZ-u8"
+        @Header("Authorization") authToken: String = token
     ): Call<TrendingResponse>
 
     @GET("movie/{movieId}")
     fun getDetails(
         @Header("accept") accept: String = "application/json",
-        @Header("Authorization") authToken: String = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NTcwYmM0MjgxY2RkZDdlNTNiNzYxY2ZlYjg0OTIyNyIsInN1YiI6IjY2NWRiOWQ1YTNmYTViYWM3MDQ5MWVlZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kyfNUr5KGnjwb-1j5oTN1637XR0raGTixdo_4IpZ-u8",
+        @Header("Authorization") authToken: String = token,
         @Path("movieId") movieId: String
     ): Call<DetailsResponse>
 }
